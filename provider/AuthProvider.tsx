44e41a95b73792a794cd4c5a8156cfb9cd7390d1
Sergio Cano <sergio.cano@disa.com>
2025-09-17 01:24:55 -0600

    Added the Secure Store for token

--- provider/AuthProvider.tsx
+++ provider/AuthProvider.tsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import * as SecureStore from "expo-secure-store";
+import { createContext, useContext, useEffect, useState } from "react";
 
 interface AuthContextType {
   isAuthenticated: boolean;
@@ -7,17 +8,31 @@ interface AuthContextType {
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
+const TOKEN_NAME = "access_token";
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
-  const logIn = () => {
+  const logIn = async () => {
+    await SecureStore.setItemAsync(TOKEN_NAME, "dummy_token");
     setIsAuthenticated(true);
   };
 
-  const logOut = () => {
+  const logOut = async () => {
+    await SecureStore.deleteItemAsync(TOKEN_NAME);
     setIsAuthenticated(false);
   };
+
+  useEffect(() => {
+    const loadAuthState = async () => {
+      try {
+        const token = await SecureStore.getItemAsync(TOKEN_NAME);
+        setIsAuthenticated(!!token);
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (error) {}
+    };
+    loadAuthState();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, logIn, logOut }}>
